52ccd28e3f048e6f1170eeac21430fedd552a2ce
Windoo <dinhhn2809@gmail.com>
2023-05-04 11:12:19 +0700

    fix review

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,6 @@
 $("#feature-work").owlCarousel({
   margin: 10,
+  items: 4,
   nav: true,
   navText: [
     "<img src='assets/images/arrow-left.png'>",
@@ -15,12 +16,19 @@ $("#feature-work").owlCarousel({
     600: {
       items: 3,
     },
+    1000: {
+      items: 3,
+    },
+    1900: {
+      items: 3,
+    },
   },
 });
 
 $("#our-team").owlCarousel({
   margin: 30,
   dots: true,
+  items: 4,
   nav: true,
   navText: [
     "<img src='/assets/images/btn-left.png'>",
